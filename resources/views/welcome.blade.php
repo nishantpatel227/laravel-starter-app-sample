35ec4c932a98946036ad7488c0f3f08fba3aafae
Nishant Patel <nishant.patel@pderas.com>
2021-07-08 15:17:37 -0600

    initial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel App 
+                    Laravel
                 </div>
 
                 <div class="links">
